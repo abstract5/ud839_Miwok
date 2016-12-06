53b0ef6159d34a8e7ecdb715192f8d8c9a5767c2
Shane Strait <abstractfive05@gmail.com>
2016-12-06 12:10:16 -0600

    Finished lesson 2

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,23 @@
+package com.example.android.miwok;
+
+/**
+ * Created by strai on 12/5/2016.
+ */
+
+public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public String getmDefaultTranslation(){
+        return mDefaultTranslation;
+    }
+
+    public String getmMiwokTranslation(){
+        return mMiwokTranslation;
+    }
+}
